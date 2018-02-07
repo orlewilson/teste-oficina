c2d98d27c6ad58a40a8e15cb624bba5129a222c5
Orlewilson Maia <prof.orlewilson@gmail.com>
2018-02-07 10:20:02 -0400

    adição dos atributos

---
+++ Oficina Git/src/br/edu/fmf/main/Gato.java
@@ -0,0 +1,17 @@
+/*
+ * Oficina Git
+ * Equipe:
+ * Data:
+ * Descrição:
+ */
+
+package br.edu.fmf.main;
+
+public class Gato {
+
+	// atributos
+	private String cor;
+	private String nome;
+	private int idade;
+	
+}
